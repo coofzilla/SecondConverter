13abc7b6d494ba06c650375225edc15c8f1bd9af
coofzilla <jeric614@yahoo.com>
2024-01-25 09:32:21 +0900

    Initial Commit

---
+++ SecondConverter/SecondConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecondConverterApp.swift
+//  SecondConverter
+//
+//  Created by Jeric Hernandez on 1/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SecondConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
