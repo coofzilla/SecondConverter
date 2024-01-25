821abe75fe92013632a3cfc3877459d7fed0100e
coofzilla <jeric614@yahoo.com>
2024-01-26 08:48:18 +0900

    feature: challenge create own converter; day 19

--- SecondConverter/ContentView.swift
+++ SecondConverter/ContentView.swift
@@ -8,14 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var seconds: Double = 3.0
+
+    // Computed properties
+    private var milliseconds: Int {
+        Int(seconds * 1_000)
+    }
+
+    private var microseconds: Int {
+        Int(seconds * 1_000_000)
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Seconds") {
+                TextField("Enter Seconds", value: $seconds, format: .number)
+                    .keyboardType(.decimalPad)
+            }
+            Section("milliseconds") {
+                Text("\(milliseconds)")
+            }
+            Section("microseconds") {
+                Text("\(microseconds)")
+            }
         }
-        .padding()
     }
 }
 
